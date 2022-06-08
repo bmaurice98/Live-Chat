28169bbe2ac20531107bec3d97ab546d7bf629b7
Brandon <bmaurice100@gmail.com>
2022-06-08 13:46:06 -0700

    Installed Chakra icons package
    Building out user profile modal and user dropdown menu

---
+++ front-end/src/components/miscellaneous/ProfileModal.jsx
@@ -0,0 +1,49 @@
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import { ChatState } from "../../context/ChatProvider";
+
+export const ProfileModal = ({ user, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton className="flex" icon={<ViewIcon />} onClick={onOpen} />
+      )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{user.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
--- front-end/src/components/miscellaneous/SideDrawer.jsx
+++ front-end/src/components/miscellaneous/SideDrawer.jsx
@@ -1,5 +1,20 @@
-import { Box, Button, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
+import { ChatState } from "../../context/ChatProvider";
+import { ProfileModal } from "./ProfileModal";
 
 export const SideDrawer = () => {
   const [search, setSearch] = useState("");
@@ -7,14 +22,51 @@ export const SideDrawer = () => {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
+  const { user } = ChatState();
+
   return (
-    <Box>
+    <Box className="w-full flex justify-between items-center bg-gray-700 py-[5px] px-[10px] border-4 border-t-0 border-gray-800 rounded-b-md">
       <Tooltip label="Search Users to chat" hasArrow placement="bottom">
         <Button variant={"ghost"} color={"whatsapp.300"}>
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <Text className="hidden md:flex p-1 ">Search User</Text>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <Text className="hidden md:flex p-1 ">User Search</Text>
         </Button>
       </Tooltip>
+      <Text className="text-2xl">Live Chat</Text>
+      <div>
+        <Menu>
+          <MenuButton className="p-1">
+            <BellIcon className="text-2xl m-1" />
+          </MenuButton>
+        </Menu>
+        <Menu>
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            variant={"ghost"}
+            color={"whatsapp.300"}
+            className="p-1"
+          >
+            <Avatar
+              size={"sm"}
+              className="cursor-pointer"
+              name={user.name}
+              src={user.image}
+            />
+          </MenuButton>
+          <MenuList>
+            <ProfileModal user={user}>
+              <MenuItem color={"whatsapp.500"} placeContent={"center"}>
+                My Profile
+              </MenuItem>
+            </ProfileModal>
+            <MenuDivider />
+            <MenuItem color={"whatsapp.500"} placeContent={"center"}>
+              Sign Out
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
     </Box>
   );
 };
